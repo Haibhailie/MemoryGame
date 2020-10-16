d591d3bd3d750f4f3b5cbea89d56278a5e1c19c3
sthanala <shashank_thanalapati@sfu.ca>
2020-10-15 21:25:14 -0700

    Splash Screen Complete (Intro screen) - Minor Edit

--- app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/IntroScreen.java
+++ app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/IntroScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class IntroScreen extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class IntroScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_screen);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Thread splashThread = new Thread() {
             public void run() {
                 try {

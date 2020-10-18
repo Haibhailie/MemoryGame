dd779798729d6deed5449ee829bd9b0679fda1ee
sthanala <shashank_thanalapati@sfu.ca>
2020-10-17 18:52:30 -0700

    Added Hyperlinks to Help Screen (not clickable for some reason)

--- app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/helpScreen.java
+++ app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/helpScreen.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class helpScreen extends AppCompatActivity {
 

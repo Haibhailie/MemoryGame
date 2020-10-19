ad47878219c585a6b3d8f32b15963d486135944d
shubhmall <skmall@sfu.ca>
2020-10-18 17:11:19 -0700

    game screen complete with basic functionality

--- app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/GameSpace.java
+++ app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/GameSpace.java
@@ -1,32 +1,50 @@
 package ca.ass3cmpt276.assignment3cmpt276;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+
+import ca.ass3cmpt276.assignment3cmpt276.model.ImpostorGrid;
 
 public class GameSpace extends AppCompatActivity {
 
-    private static final int NUM_COLS = 5;
-    private static final int NUM_ROWS = 5;
+    private static final int NUM_ROWS = 4;
+    private static final int NUM_COLS = 6;
+    private static final int DEFAULT_ROW = 4;
+    private static final int DEFAULT_COLUMN = 6;
+    private static final int DEFAULT_IMPOSTOR_COUNT = 6;
 
     Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
+    ImpostorGrid impostorGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_space);
 
-
+        impostorGrid = ImpostorGrid.getInstance(DEFAULT_ROW ,
+                DEFAULT_COLUMN , DEFAULT_IMPOSTOR_COUNT);
         populateButtons();
+        displayStats();
+    }
+
+    private void displayStats() {
+        TextView textFoundImpostors = (TextView) findViewById(R.id.textFoundImpostors);
+        textFoundImpostors.setText("Found " + impostorGrid.getImpostorsFound() + " of " +
+                impostorGrid.getImpostorCount() + " Impostors");
+
+        TextView textScanUsed = (TextView) findViewById(R.id.textScansUsed);
+        textScanUsed.setText("" + impostorGrid.getScanCount() + " Scans used");
     }
 
     private void populateButtons() {
@@ -65,6 +83,64 @@ public class GameSpace extends AppCompatActivity {
     }
 
     private void gridButtonClicked(int row, int col) {
+
+        // update all buttons clicked
+        Button button = buttons[row][col];
+        // Lock button sizes:
+        lockButtonSizes();
+        int checkAction = impostorGrid.onGridClicked(row, col);
+
+        if(checkAction == 1){
+            // display impostor
+            int newWidth = button.getWidth();
+            int newHeight = button.getHeight();
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.orange);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+            Resources resource = getResources();
+            button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+            updateAllGrid(row, col);
+        }
+
+        else if(checkAction == 0){
+            // display number of impostors in rows and columns
+            button.setText("" + impostorGrid.getImpostorInRowsAndColumns(row, col));
+            button.setPadding(0, 0, 0, 0);
+        }
+        displayStats();
+    }
+
+    private void updateAllGrid(int row, int col) {
+        Button button;
+        for(int c = 0; c < NUM_COLS; c ++){
+            if(impostorGrid.getGridValue(row, c) == 2) {
+                button = buttons[row][c];
+                button.setText("" + impostorGrid.getImpostorInRowsAndColumns(row, c));
+                button.setPadding(0, 0, 0, 0);
+            }
+        }
+        for(int r = 0; r< NUM_ROWS; r++){
+            if(impostorGrid.getGridValue(r, col) == 2) {
+                button = buttons[r][col];
+                button.setText("" + impostorGrid.getImpostorInRowsAndColumns(r, col));
+                button.setPadding(0, 0, 0, 0);
+            }
+        }
+    }
+
+    private void lockButtonSizes() {
+        for(int i = 0; i < NUM_ROWS; i++){
+            for(int j = 0; j < NUM_COLS; j++){
+                Button button = buttons[i][j];
+
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getWidth();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
+            }
+        }
 
     }
 }
--- app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/OptionsActivity.java
+++ app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/OptionsActivity.java
@@ -10,7 +10,15 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toolbar;
 
+import ca.ass3cmpt276.assignment3cmpt276.model.ImpostorGrid;
+
 public class OptionsActivity extends AppCompatActivity {
+    private static final int DEFAULT_ROW_VALUE = 4;
+    private static final int DEFAULT_COLUMN_VALUE = 6;
+    private static final int DEFAULT_IMPOSTOR_COUNT = 6;
+    private int row;
+    private int column;
+    private int impostorCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +34,97 @@ public class OptionsActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        Button backButton = findViewById(R.id.backButton);
+        setupBackButton();
+        setDefaultValue();
+        getGridOptions();
+        getImpostorCountOption();
+    }
+
+    private void setupBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImpostorGrid.getInstance(row, column, impostorCount);
                 finish();
+            }
+        });
+    }
+
+    private void setDefaultValue() {
+        row = DEFAULT_ROW_VALUE;
+        column = DEFAULT_COLUMN_VALUE;
+        impostorCount = DEFAULT_IMPOSTOR_COUNT;
+    }
+
+    private void getImpostorCountOption() {
+        Button no6 = (Button) findViewById(R.id.no6);
+        no6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                impostorCount = 6;
+            }
+        });
+
+        Button no10 = (Button) findViewById(R.id.no10);
+        no6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                impostorCount = 10;
+            }
+        });
+
+        Button no15 = (Button) findViewById(R.id.no15);
+        no6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                impostorCount = 15;
+            }
+        });
+
+        Button no20 = (Button) findViewById(R.id.no20);
+        no6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                impostorCount = 20;
+            }
+        });
+    }
+
+    public void getGridOptions(){
+        Button x46 = (Button) findViewById(R.id.x46);
+        x46.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                row = 4;
+                column = 6;
+            }
+        });
+
+        Button x510 = (Button) findViewById(R.id.x510);
+        x510.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                row = 5;
+                column = 10;
+            }
+        });
+
+        Button x615 = (Button) findViewById(R.id.x615);
+        x615.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                row = 6;
+                column = 15;
+            }
+        });
+
+        Button x718 = (Button) findViewById(R.id.x718);
+        x718.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                row = 7;
+                column = 18;
             }
         });
     }

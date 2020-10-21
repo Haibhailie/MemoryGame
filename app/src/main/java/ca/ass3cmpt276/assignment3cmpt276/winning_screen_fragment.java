fbd3b60aac10158e0b907aa08de49de19e20bf6b
shubhmall <skmall@sfu.ca>
2020-10-20 17:50:58 -0700

    added comments on winning_screen_fragment (alert dialog) class

--- app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/winning_screen_fragment.java
+++ app/src/main/java/ca/ass3cmpt276/assignment3cmpt276/winning_screen_fragment.java
@@ -13,7 +13,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.google.android.material.internal.ViewUtils;
-
+/*
+* Fragment handling the victory message on exit of GameSpace activity
+ */
 public class winning_screen_fragment extends AppCompatDialogFragment {
 
     @Override
